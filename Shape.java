61b209995f803a5e94b20e3789e11ab2f8fdf1f5
osama0211 <75998061+osama0211@users.noreply.github.com>
2024-03-14 21:59:53 +0000

    creating shape class

---
+++ Shape.java
@@ -0,0 +1,26 @@
+package griffith;
+abstract class Shape {
+    private String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Abstract methods
+    public abstract double area();
+
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
